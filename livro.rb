92b197c53930057c144dbdd4916f228d928386d4
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-02-01 20:11:56 -0300

    Exercicio 02

--- livro.rb
+++ livro.rb
@@ -1,33 +1,42 @@
 class Livro
     attr_reader :titulo, :preco, :ano_lancamento
-    def initialize(titulo, preco, ano_lancamento)
-        @titulo = titulo
-        @preco = preco
+    #attr_writer :preco
+
+    def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
+        @titulo = titulo        
         @ano_lancamento = ano_lancamento
+        @preco = calcula_preco preco
+        @possui_reimpressao = possui_reimpressao
+    end
+
+    private
+
+    def calcula_preco(base)
+        if @ano_lancamento < 2000
+            base *= 0.7 
+        else 
+            base    
+        end
+    end
+
+    def possui_reimpressao?
+        @possui_reimpressao
     end
 end
 
-livro_rails = Livro.new(
-    "Agile Web Developmento with Rails",
-    70,
-    2011
+algoritmos = Livro.new(
+    "Algoritmos",
+    100,
+    1998,
+    true
 )
 
-livro_ruby = Livro.new(
-    "Programmin Ruby 1.9",
-    60,
-    2010
-)
-
-livros = [
-    livro_rails,
-    livro_ruby
-]
-
-def imprime_nota_fiscal livros
-  livros.each do |livro|
-    puts "Titulo: #{livro.titulo} - #{livro.preco}"
-  end  
+def livro_para_newsletter livro
+    if livro.ano_lancamento < 1999
+        puts "Newsletter/Liquidação"
+        puts livro.titulo
+        puts livro.preco
+    end
 end
 
-imprime_nota_fiscal livros
+livro_para_newsletter algoritmos
