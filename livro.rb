40f89e2a02c40469724d12f7fd8c735bdbdfa533
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-02-08 20:45:53 -0300

    Exercicio 03

--- livro.rb
+++ livro.rb
@@ -5,22 +5,59 @@ class Livro
     def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
         @titulo = titulo        
         @ano_lancamento = ano_lancamento
-        @preco = calcula_preco preco
         @possui_reimpressao = possui_reimpressao
+        @preco = calcula_preco preco
+    end
+
+    def to_csv
+        "#{@titulo}, #{@ano_lancamento}, #{@preco}"
     end
 
     private
 
     def calcula_preco(base)
-        if @ano_lancamento < 2000
-            base *= 0.7 
-        else 
-            base    
+        if @ano_lancamento < 2006
+            if possui_reimpressao?
+                base * 0.9
+            else
+                base * 0.95
+            end
+        
+        elsif @ano_lancamento <= 2010
+            base * 0.96
+        else
+            base
         end
     end
 
     def possui_reimpressao?
         @possui_reimpressao
+    end
+end
+
+class Estoque
+    def initialize
+        @livros = []
+    end
+
+    def exporta_csv 
+        @livros.each do |livro|
+            puts livro.to_csv
+        end    
+    end
+
+    def mais_barato_que valor
+        @livros.select do |livro|
+            livro.preco <= valor
+        end
+    end
+
+    def total
+        @livros.size
+    end
+
+    def adiciona livro
+        @livros << livro if livro
     end
 end
 
@@ -31,12 +68,23 @@ algoritmos = Livro.new(
     true
 )
 
-def livro_para_newsletter livro
-    if livro.ano_lancamento < 1999
-        puts "Newsletter/Liquidação"
-        puts livro.titulo
-        puts livro.preco
-    end
-end
+arquitetura = Livro.new(
+    "Introdução a arquitetura e design de software",
+    70,
+    2011,
+    true
+)
 
-livro_para_newsletter algoritmos
+estoque = Estoque.new
+estoque.adiciona algoritmos
+estoque.adiciona arquitetura
+estoque.adiciona Livro.new("The Pragmatic Programmer", 100, 1999, true)
+estoque.adiciona Livro.new("Programming Ruby", 100, 2004, true)
+estoque.adiciona nil
+
+estoque.exporta_csv
+
+baratos = estoque.mais_barato_que 80
+baratos.each do |livro|
+    puts livro.titulo
+end
